11336d6b40f58e385cd293f4e0f991d2e44a6bdb
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-07-02 09:05:55 +0700

    update Attribute.php membuat attribute via reflection

--- Attribute.php
+++ Attribute.php
@@ -19,9 +19,31 @@
   {
     $class = new ReflectionClass($object);
     $properties = $class->getProperties();
-    
+
     foreach($properties as $property)
     {
       validateNotBlank($property, $object);
     }
   }
+
+  function validateNotBlank(ReflectionProperty $property, object $object): void
+  {
+    $attributes = $property->getAttributes(NotBlank::class);
+    if(count($attributes) > 0)
+    {
+      if(!$property->isInitialized($object))
+      {
+        throw new Exception("Property $property->name is null");
+      }
+
+      if($property->getValue($object) == null)
+      {
+        throw new Exception("Property $property->name is null");
+      }
+    }
+  }
+
+  $request = new LoginRequest();
+  $request->username = "eko";
+  $request->password = null;
+  validate($request);
