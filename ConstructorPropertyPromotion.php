99f8a189630c8095b3ceba99294e3429d61f52eb
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-07-02 12:31:30 +0700

    update ConstructorPropertyPromotion.php

--- ConstructorPropertyPromotion.php
+++ ConstructorPropertyPromotion.php
@@ -2,16 +2,13 @@
 
   class Product
   {
-    public string $id;
-    public string $name;
-    public int $price;
-    public int $quantity;
-
-    public function __construct(string $id, string $name, int $price, int $quantity)
+    public function __construct(public string $id, public string $name,
+    public int $price = 0, public int $quantity = 0, private bool $expensive = false)
     {
-      $this->id = $id;
-      $this->name = $name;
-      $this->price = $price;
-      $this->quantity = $quantity;
+
     }
   }
+
+  $product = new Product(id: "1", name: "Mie Ayam", price: 15000);
+  var_dump($product);
+  echo $product->name . PHP_EOL;
