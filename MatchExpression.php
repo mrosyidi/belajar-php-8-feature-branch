ef61648ebea0a2759d4d1fe147001881903d11a0
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-07-02 21:38:21 +0700

    update MatchExpression.php

--- MatchExpression.php
+++ MatchExpression.php
@@ -21,3 +21,14 @@
   }
 
   echo $result . PHP_EOL;
+
+  $value = "A";
+  $result = match($value)
+  {
+    "A", "B", "C" => "Anda lulus",
+    "D" => "Anda tidak lulus",
+    "E" => "Sepertinya Anda salah jurusan",
+    default => "Nilai apa itu?"
+  };
+
+  echo $result . PHP_EOL;
